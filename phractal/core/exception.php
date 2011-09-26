23c1eaafa8d6dd2396f5af7237a7ee9897fe87f3
Matthew Barlocker <mbarlocker@gmail.com>
2011-09-26 16:51:54 -0600

    create PhractalException

---
+++ phractal/core/exception.php
@@ -0,0 +1,24 @@
+<?php if (!defined('BASEPATH')) { exit(-1); }
+/**
+ * phractal
+ *
+ * A framework for PHP 5 dedicated to high availability and scaling.
+ *
+ * @author		Matthew Barlocker
+ * @copyright	Copyright (c) 2011, Matthew Barlocker
+ * @license		Proprietary, All Rights Reserved
+ * @link		https://github.com/mbarlocker/phractal
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Phractal Exception
+ *
+ * This class is the parent class for all exceptions within the
+ * phractal framework.
+ */
+abstract class PhractalException extends Exception
+{
+}
