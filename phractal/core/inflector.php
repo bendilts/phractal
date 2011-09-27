f902e200f479a92e4e14f0b91edb486a31bf890a
Matthew Barlocker <mbarlocker@gmail.com>
2011-09-26 20:34:09 -0600

    write PhractalInflector class

---
+++ phractal/core/inflector.php
@@ -0,0 +1,58 @@
+<?php if (!defined('PHRACTAL')) { exit(-1); }
+/**
+ * phractal
+ *
+ * A framework for PHP 5 dedicated to high availability and scaling.
+ *
+ * @author		Matthew Barlocker
+ * @copyright	Copyright (c) 2011, Matthew Barlocker
+ * @license		Proprietary, All Rights Reserved
+ * @link		https://github.com/mbarlocker/phractal
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Inflector Class
+ *
+ * Manipulates strings to match conventions. For example,
+ * can change PhractalInflector to phractal_inflector.php,
+ * and vice versa.
+ */
+class PhractalInflector extends PhractalObject
+{
+	/**
+	 * Change a string into a camel cased word (ie myClassName)
+	 * 
+	 * @param string $string
+	 * @return string
+	 */
+	public function camel($string)
+	{
+		return str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
+	}
+	
+	/**
+	 * Change a string into an upper, camel cased word (ie MyClassName)
+	 * 
+	 * @param string $string
+	 * @return string
+	 */
+	public function camel_upper_first($string)
+	{
+		$new = $this->camel($string);
+		return empty($new) ? '' : strtoupper($new[0]) . substr($new, 1);
+	}
+	
+	/**
+	 * Change a string into an underscored word (ie my_class_name)
+	 * 
+	 * @param string $string
+	 * @return string
+	 */
+	public function underscore($string)
+	{
+		return strtolower(preg_replace('/(?<=\\w)([A-Z])/', '_\\1', $string));
+	}
+}
