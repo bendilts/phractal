e5d18743944b3c446351693ba4bb301edfe755cd
Matthew Barlocker <mbarlocker@gmail.com>
2011-09-28 15:13:17 -0600

    first stab at PhractalLoader

---
+++ phractal/core/loader.php
@@ -0,0 +1,100 @@
+<?php if (!defined('PHRACTAL')) { exit('no access'); }
+/**
+ * phractal
+ *
+ * A framework for PHP 5 dedicated to high availability and scaling.
+ *
+ * @author		Matthew Barlocker
+ * @copyright	Copyright (c) 2011, Matthew Barlocker
+ * @license		Proprietary, All Rights Reserved
+ * @link		https://github.com/mbarlocker/phractal
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Loader Class
+ *
+ * Including an autoload registration, this class is in charge
+ * of loading all classes (excluding phractal core classes).
+ */
+class PhractalLoader extends PhractalObject
+{
+	/**
+	 * When true, this object is one of the registered
+	 * autoloaders in the PHP runtime.
+	 * @var bool
+	 */
+	protected $registered = false;
+	
+	/**
+	 * Registers the autoload function on this class.
+	 */
+	public function register()
+	{
+		if (!$this->registered)
+		{
+			spl_autoload_register(array($this, 'autoload'), false, false);
+			$this->registered = true;
+		}
+	}
+	
+	/**
+	 * Unregister the autoload function on this class.
+	 */
+	public function unregister()
+	{
+		if ($this->registered)
+		{
+			spl_autoload_unregister(array($this, 'autoload'));
+			$this->registered = false;
+		}
+	}
+	
+	/**
+	 * Autoload a class based on name.
+	 * 
+	 * This function is the subject of spl_autoload_register. It does
+	 * not work for:
+	 *     - Phractal Core Classes
+	 *     - Migration Classes
+	 *     - Third Party Classes
+	 * 
+	 * @param string $classname Name of the class to load
+	 */
+	public function autoload($classname)
+	{
+		static $suffix_map = array(
+			'Controller' => 'controller/controllers',
+			'Component'  => 'controller/components',
+			'Driver'     => 'model/drivers',
+			'Model'      => 'model/models',
+			'Record'     => 'model/records',
+			'Manager'    => 'model/managers',
+			'Helper'     => 'view/helpers',
+			'Renderer'   => 'view/renderers',
+		);
+		
+		$core = substr($classname, 0, 8) === 'Phractal';
+		$base = $core ? PATH_PHRACTAL : PATH_APP;
+		
+		if ($core)
+		{
+			$classname = substr($classname, 8);
+		}
+		
+		foreach ($suffix_map as $suffix => $path)
+		{
+			$suffix_length = strlen($suffix);
+			if (substr($classname, -$suffix_length) === $suffix)
+			{
+				// TODO: This doesn't account for subclasses like MysqlDriver, ApcCacheComponent, etc.
+				$classname = substr($classname, 0, strlen($classname) - $suffix_length);
+				$underscored = Phractal::get_instance()->get_inflector()->underscore($classname);
+				require_once($base . '/' . $path . '/' . $underscored . '.php');
+				break;
+			}
+		}
+	}
+}
