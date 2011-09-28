29383444868d4427d627387175c38f8d75021ac6
Matthew Barlocker <mbarlocker@gmail.com>
2011-09-28 15:12:58 -0600

    make empty classes for benchmark and logging core classes

---
+++ phractal/core/benchmark.php
@@ -0,0 +1,23 @@
+<?php if (!defined('PHRACTAL')) { exit('no access'); }
+/**
+ * phractal
+ *
+ * A framework for PHP 5 dedicated to high availability and scaling.
+ *
+ * @author		Matthew Barlocker
+ * @copyright	Copyright (c) 2011, Matthew Barlocker
+ * @license		Proprietary, All Rights Reserved
+ * @link		https://github.com/mbarlocker/phractal
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Benchmark Class
+ *
+ * Tracks resource usage during runtime.
+ */
+class PhractalBenchmark extends PhractalObject
+{
+}
