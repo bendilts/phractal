59096941436ed3834f4e3f43bd3734d64972a40d
Matthew Barlocker <mbarlocker@gmail.com>
2011-11-27 18:49:36 -0700

    copyright on files

--- app/controller/controllers/base.php
+++ app/controller/controllers/base.php
@@ -1,5 +1,23 @@
-<?php
+<?php if (!defined('PHRACTAL')) { exit('no access'); }
+/**
+ * phractal
+ *
+ * A framework for PHP 5 dedicated to high availability and scaling.
+ *
+ * @author		Matthew Barlocker
+ * @copyright	Copyright (c) 2011, Matthew Barlocker
+ * @license		Proprietary, All Rights Reserved
+ * @link		https://github.com/mbarlocker/phractal
+ * @filesource
+ */
 
-class BaseController extends PhractalBaseController
+// ------------------------------------------------------------------------
+
+/**
+ * Base Controller
+ * 
+ * App specific base class for all controller classes
+ */
+abstract class BaseController extends PhractalBaseController
 {
 }
--- app/model/managers/base.php
+++ app/model/managers/base.php
@@ -1,4 +1,17 @@
-<?php
+<?php if (!defined('PHRACTAL')) { exit('no access'); }
+/**
+ * phractal
+ *
+ * A framework for PHP 5 dedicated to high availability and scaling.
+ *
+ * @author		Matthew Barlocker
+ * @copyright	Copyright (c) 2011, Matthew Barlocker
+ * @license		Proprietary, All Rights Reserved
+ * @link		https://github.com/mbarlocker/phractal
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
 
 class BaseManager extends PhractalBaseManager
 {
